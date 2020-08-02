9f03739a28d92c06021d13e91fced2cb98a4c725
pashapurecode <69114189+pashapurecode@users.noreply.github.com>
2020-08-02 20:50:17 +0530

    Update ChromeRunner.java

--- src/test/java/Runners/ChromeRunner.java
+++ src/test/java/Runners/ChromeRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = { "src/test/resources/Features/chrome_flight_booking.feature" },
-        glue = {"StepsDefinitions" }
+        glue = {"test.java.StepsDefinitions" }
 )
 public class ChromeRunner extends AbstractTestNGCucumberTests {
 	
